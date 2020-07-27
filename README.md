6905cc61d4d3d6a79e8bf06a5cae6813d8084276
dalekatwork <pooja.maheshwari247@gmail.com>
2020-07-27 23:17:47 +0530

    working soln for time picker

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # time_picker
 
-This is a time picker library in which you can set an interval in HH:MM
+This is a time picker library in which you can set an interval in HH:MM. It can be used to pick interval using a slider type definition.
+
+## Usage
+
+```dart
+
+// add immediately after class definition
+int _hour = 0;
+int _minute = 0;
+
+void _fetchValues(int newHour, int newMinute) {
+  setState(() {
+    _hour = newHour;
+    _minute = newMinute;
+  });
+}
+
+
+// use in render
+TimePicker(fetchValues: _fetchValues)
+```
+
+## Demo
+
+<img src="./demo/demo.gif" width="360" height="650"/>
--- lib/components/index.dart
+++ lib/components/index.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
 
 class TimePicker extends StatefulWidget {
-  const TimePicker({Key key, this.title}) : super(key: key);
+  const TimePicker({
+    Key key,
+    this.title,
+    this.fetchValues,
+  }) : super(key: key);
+
+  final Function fetchValues;
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -31,34 +37,35 @@ class _TimePicker extends State<TimePicker> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Row(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        children: <Widget>[
-          Row(children: <Widget>[
-            Column(
-              children: <Widget>[
-                NumberPicker.integer(
-                    initialValue: _hourCounter,
-                    minValue: 0,
-                    maxValue: 24,
-                    onChanged: (num newValue) =>
-                        _hourCounter = newValue as int),
-                const Text('HH')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                NumberPicker.integer(
-                    initialValue: _minuteCounter,
-                    minValue: 0,
-                    maxValue: 60,
-                    onChanged: (num newValue) =>
-                        _minuteCounter = newValue as int),
-                const Text('MM')
-              ],
-            ),
-          ]),
-        ]);
+    return Row(children: <Widget>[
+      Row(children: <Widget>[
+        Column(
+          children: <Widget>[
+            NumberPicker.integer(
+                initialValue: _hourCounter,
+                minValue: 00,
+                maxValue: 24,
+                onChanged: (num newValue) {
+                  setState(() => _hourCounter = newValue as int);
+                  widget.fetchValues(_hourCounter, _minuteCounter);
+                }),
+            const Text('HH')
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            NumberPicker.integer(
+                initialValue: _minuteCounter,
+                minValue: 00,
+                maxValue: 60,
+                onChanged: (num newValue) {
+                  setState(() => _minuteCounter = newValue as int);
+                  widget.fetchValues(_hourCounter, _minuteCounter);
+                }),
+            const Text('MM')
+          ],
+        ),
+      ]),
+    ]);
   }
 }
